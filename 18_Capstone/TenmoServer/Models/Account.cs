3a9628cf6e329615bf89830d02ccd9d31bb17175
Keith Wier <wier.keith@gmail.com>
2020-11-04 12:09:35 -0500

    Classes started

---
+++ 18_Capstone/TenmoServer/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public int UserID { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
---
+++ 18_Capstone/TenmoServer/Models/Transfer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TenmoServer.Models
+{
+    public class Transfer
+    {
+        public int TransferID {get ; set;}
+        public int AccountFrom { get; set; }
+        public int AccountTo { get; set; }
+        public int TransferTypeID { get; set; }
+        public decimal Amount { get; set; }
+        public int TransferStatusID { get; set; }
+    }
+}
