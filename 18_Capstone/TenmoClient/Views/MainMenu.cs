4512d6bd2f6c217e4cb72684216db6a3de246f12
Mike Morel <mike@techelevator.com>
2020-11-03 21:32:25 -0500

    Module 2 Capstone initial push

---
+++ 18_Capstone/TenmoClient/Views/MainMenu.cs
@@ -0,0 +1,65 @@
+﻿using MenuFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TenmoClient.Data;
+
+namespace TenmoClient.Views
+{
+    public class MainMenu : ConsoleMenu
+    {
+
+        public MainMenu()
+        { 
+            AddOption("View your current balance", ViewBalance)
+                .AddOption("View your past transfers", ViewTransfers)
+                .AddOption("View your pending requests", ViewRequests)
+                .AddOption("Send TE bucks", SendTEBucks)
+                .AddOption("Request TE bucks", RequestTEBucks)
+                .AddOption("Log in as different user", Logout)
+                .AddOption("Exit", Exit);
+        }
+
+        protected override void OnBeforeShow()
+        {
+            Console.WriteLine($"TE Account Menu for User: {UserService.GetUserName()}");
+        }
+
+        private MenuOptionResult ViewBalance()
+        {
+            Console.WriteLine("Not yet implemented!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult ViewTransfers()
+        {
+            Console.WriteLine("Not yet implemented!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult ViewRequests()
+        {
+            Console.WriteLine("Not yet implemented!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult SendTEBucks()
+        {
+            Console.WriteLine("Not yet implemented!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult RequestTEBucks()
+        {
+            Console.WriteLine("Not yet implemented!");
+            return MenuOptionResult.WaitAfterMenuSelection;
+        }
+
+        private MenuOptionResult Logout()
+        {
+            UserService.SetLogin(new API_User()); //wipe out previous login info
+            return MenuOptionResult.CloseMenuAfterSelection;
+        }
+
+    }
+}
