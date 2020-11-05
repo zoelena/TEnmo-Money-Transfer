d742051cd071123db6ac8d7bf885338634bef320
Zoe Moskalew <zoe.moskalew@gmail.com>
2020-11-05 12:31:51 -0500

    push 8

--- 18_Capstone/TenmoClient/DAO/AuthService.cs
+++ 18_Capstone/TenmoClient/DAO/AuthService.cs
@@ -8,7 +8,7 @@ namespace TenmoClient
     public class AuthService
     {
         private readonly static string API_BASE_URL = "https://localhost:44315/";
-        private readonly IRestClient client = new RestClient();
+        private static IRestClient client = new RestClient();
 
         //login endpoints
         public bool Register(LoginUser registerUser)
