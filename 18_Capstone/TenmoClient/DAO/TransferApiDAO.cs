4ffc6ee065e3c3b7779d8ae7bdef33c24a6bf033
Zoe Moskalew <zoe.moskalew@gmail.com>
2020-11-05 19:08:57 -0500

    Push 10

---
+++ 18_Capstone/TenmoClient/DAO/TransferApiDAO.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TenmoClient.Data;
+
+namespace TenmoClient.DAO
+{
+    class TransferApiDAO
+    {
+        private readonly static string API_BASE_URL = "https://localhost:44315/";
+        private readonly IRestClient client = new RestClient();
+        //private static API_Account account = new API_Account();
+        //private object registerUser;
+
+        public List<Transfer> GetTransfers(int userId)
+        {
+            RestRequest request = new RestRequest($"{API_BASE_URL}transfers/{userId}");
+            IRestResponse<List<Transfer>> response = client.Get<List<Transfer>>(request);
+            List<Transfer> transferList = response.Data;
+            return transferList;
+
+        }
+
+    }
+}
--- 18_Capstone/TenmoClient/Models/Transfer.cs
+++ 18_Capstone/TenmoClient/Models/Transfer.cs
@@ -7,11 +7,15 @@ namespace TenmoClient.Data
     class Transfer
     {
         public int TransferID { get; set; }
+        public int TransferTypeID { get; set; }
+        public int TransferStatusID { get; set; }
         public int AccountFrom { get; set; }
         public int AccountTo { get; set; }
-        public int TransferTypeID { get; set; }
         public decimal Amount { get; set; }
-        public int TransferStatusID { get; set; }
+        public string FromName { get; set; }
+        public string ToName { get; set; }
+        public string TypeName { get; set; }
+        public string StatusName { get; set; }
 
     }
 }
