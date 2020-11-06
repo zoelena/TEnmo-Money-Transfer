a00dc5c543cdd965401f1a889b3c6f7073cab0da
Keith Wier <wier.keith@gmail.com>
2020-11-06 18:03:00 -0500

    11/6 update 5

---
+++ 18_Capstone/TenmoServer/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TenmoServer.DAO;
+using TenmoServer.Models;
+
+namespace TenmoServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private IUserDAO userDAO;
+        public UserController(IUserDAO _userDAO)
+        {
+            userDAO = _userDAO;
+        }
+
+        [HttpGet]
+        public List<User> GetAllUsers()
+        {
+            return userDAO.GetUsers();
+        }
+    }
+}
